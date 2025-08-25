6e0030b9fc3fc8790aa9583a240a53dfd9ef3a65
Aravindakrishnan <95763443+arkhiVd@users.noreply.github.com>
2025-08-25 03:55:48 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "ip_hash_secret" {
   description = "A secret salt used for hashing visitor IP addresses."
   type        = string
-  sensitive   = true # This tells Terraform not to show the value in logs
+  sensitive   = true
 }
