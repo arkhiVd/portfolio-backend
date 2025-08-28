6496a5a1a93cd010f2ea77ad6b45486cdde26bb1
arkhiVd <95763443+arkhiVd@users.noreply.github.com>
2025-08-28 05:54:48 +0000

    feat: Add Prometheus monitoring layer and metrics endpoint

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
 terraform {
-  required_version = ">= 1.8.0"
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.45"
+      version = "~> 5.45.0"
     }
   }
+}
+
+provider "aws" {
+  region = "ap-south-2"
 }
 
 terraform {
@@ -15,13 +19,7 @@ terraform {
     region = "ap-south-2"
   }
 }
-provider "aws" {
-  region = "ap-south-2"
 
-  assume_role {
-    role_arn = "arn:aws:iam::615201069679:role/GitHub_Terraform_Backend_Deployer_Role"
-  }
-}
 data "aws_caller_identity" "current" {}
 
 # tfsec:ignore:aws-dynamodb-table-customer-key
